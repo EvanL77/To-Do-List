d7e486a608d7310606daeb1220943eb21405b4fa
EvanL77 <150301463+EvanL77@users.noreply.github.com>
2024-08-08 22:41:09 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const inputBox = document.getElementById("input");
 const list = document.getElementById("list");
+const canvas = document.querySelector('confetti')
+let count = 0;
+
+const jsConfetti = new JSConfetti()
 
 function addTask(){
     if(inputBox.value === ''){
@@ -9,10 +13,19 @@ function addTask(){
         let li = document.createElement("li")
         li.innerHTML = inputBox.value;
         list.appendChild(li);
+        count++;
     }
 }
 list.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("done");
+        count--;
+        allDone();
     }
 },false);
+
+function allDone(){
+    if(count===0){
+        jsConfetti.addConfetti()
+    }
+}
